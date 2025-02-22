45af54ad824e46e9718730a6093289d0a014592e
ayodeji oni <ayodejioni1505@gmail.com>
2025-02-22 13:59:26 +0100

    feat: integration.json and tick URL

---
+++ api/routes/integration.py
@@ -0,0 +1,152 @@
+import random
+from fastapi import APIRouter, Request, status
+import httpx
+from pydantic import BaseModel
+from fastapi.responses import JSONResponse
+
+router = APIRouter()
+
+class TelexSetting(BaseModel):
+    label: str
+    type: str
+    required: bool
+    default: str
+
+
+class TelexPayload(BaseModel):
+    channel_id: str
+    return_url: str
+    settings: list[TelexSetting]
+
+
+class PheidippidesPayload(BaseModel):
+    message: str = "Could not resolve"
+    username: str = "Pheidippides API"
+    event_name: str = "Daily Book Recommendation"
+    status: str = "success"
+
+
+#telex integration.json endpoint
+@router.get("/integration.json")
+def integration_json(request: Request):
+    base_url = str(request.base_url).rstrip("/")
+    return {
+        "data": {
+            "date": {
+                "created_at": "2025-02-21",
+                "updated_at": "2025-02-21"
+                },
+            "descriptions": {
+                "app_name": "Pheidippides API",
+                "app_description": "An integration that suggests books to read based on any selected genre.",
+                "app_logo": "https://i.imgur.com/D2619X4.jpeg",
+                "app_url": f"{base_url}/pheidippides-api", #change to an env variable
+                "background_color": "#fff"
+            },
+            "is_active": True,
+            "integration_type": "interval",
+            "integration_category": "Communication & Collaboration",
+            "key_features": [
+                "daily-book-recommendations"
+                ],
+            "author": "Ayodeji Oni",
+            "settings": [
+                {
+                    "label": "genre",
+                    "type": "dropdown",
+                    "required": True,
+                    "default": "fantasy",
+                    "options": [
+                            # "random", #issues with generating random genre on google api
+                            "fantasy",
+                            "mystery",
+                            "horror",
+                            "romance",
+                            "dystopian",
+                            "adventure",
+                            "biography",
+                            "history",
+                            "comedy",
+                            "nigerian"
+                        ]
+                },
+                {
+                    "label": "interval",
+                    "type": "text",
+                    "required": True,
+                    "default": "* * * * *"
+                }
+            ],
+            # "target_url": "\"\"",
+            "tick_url": f"{base_url}/pheidippides-api/tick" #change to an env variable
+        }
+    }
+
+
+#Implemetation to get genre from request payload
+async def get_recommendation(telex_payload: TelexPayload):
+    #Note: return_url for telx
+    
+    #getting the selected genre
+    genre = [s.default for s in telex_payload.settings if s.label == "genre"]
+    genre = str(genre[0])
+
+    #setting a limit to the number of items returned
+    max_results = limit = 20
+    
+    #public api used for making queries
+    API = {
+        "google-books-api": f"https://www.googleapis.com/books/v1/volumes?q=subject:{genre}&maxResults={max_results}",
+        "open-library-api": f"https://openlibrary.org/subjects/{genre}.json?limit={limit}"
+    }
+
+    chosen_api = API[random.choice(list(API.keys()))]
+       
+    async with httpx.AsyncClient() as client:
+        try:
+            response = await client.get(chosen_api)
+            texts = response.json()
+        except: 
+            return JSONResponse({"error": "Public API request failed"})
+
+    if chosen_api == API["google-books-api"]:
+        #data extraction
+        books = texts.get("items", [])
+        random_book = random.choice(list(books))
+        detail = random_book.get("volumeInfo")
+        title = detail.get("title")
+        authors = ", ".join(detail.get("authors"))
+        published_date = detail.get("publishedDate")
+        description = detail.get("description")
+        
+    elif chosen_api == API["open-library-api"]:
+        #data extraction
+        books = texts.get("works", [])
+        random_book = random.choice(list(books))
+        title = random_book.get("title")
+        authors = random_book.get("authors")[0].get("name")
+        published_date = random_book.get("first_publish_year")
+        description = ", ".join(random_book.get("subject")[0:3])
+
+    pheidippides_message = f"Title: {title}\nAuthor(s): {authors}\nYear: {published_date}\nDescription: {description}"
+
+    payload = PheidippidesPayload(message=pheidippides_message)
+
+    async with httpx.AsyncClient() as client:
+        try:
+            response = await client.post(
+                "https://ping.telex.im/v1/webhooks/0195057a-ebc9-7646-af52-41800fa80490", #change to channel_url
+                json=payload.model_dump(),
+                headers={
+                    "Accept" : "application/json",
+                    "Content-Type": "application/json"}
+            )
+            return response.json()
+        except:
+            return {"error": "Error communicating with telex"}
+        
+
+#telex tick_url endpoint
+@router.post("/tick")
+async def tick_url(telex_payload: TelexPayload):
+    return await get_recommendation(telex_payload)
